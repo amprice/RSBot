69fcad8fa15e8da33418183292d195b7fdfdd25e
Andrew Price <am_price@icloud.com>
2022-10-29 00:18:32 +1100

    Add support for slash commands

--- src/RSBot.py
+++ src/RSBot.py
@@ -1,21 +1,27 @@
+# from typing import TYPE_CHECKING
 from inspect import getmembers
 from sys import prefix
-import discord
-from discord import Reaction, User
+
 import os
+from urllib import response
 import RSQueueManager
 import discord.utils
 import asyncio 
 import logging
-
+from typing import *
 from botenv import getEnvKey
 from botversion import getVersion
 
 import command
 from command_help import HelpCommand
-from command_ver import VersionCommand
 
+import discord
+from discord import Reaction, User, types, app_commands
 from discord.ext import commands
+
+#from discord.ext.commands.bot import PrefixType
+
+from discord.ext.commands._types import *
 
 from RSQueueManager import RSQueueManager
 from modules.BotInfo.cog import BotInfo
@@ -23,14 +29,26 @@ from modules.BotInfo.cog import BotInfo
 logger = logging.getLogger('discord')
 logging.basicConfig(level=logging.INFO)
 
+
 help_cmd = commands.DefaultHelpCommand(show_parameter_descriptions=False)
-client : commands.Bot = commands.Bot(intents=discord.Intents.all(), command_prefix="-", help_command=help_cmd)
+client : commands.Bot = commands.Bot(intents=discord.Intents.all(), 
+									 command_prefix="-", 
+									 help_command=help_cmd,
+									 guilds = [discord.Object(id=939859311847415818)])
 
 cm = command.Command_Manager()
 
+@commands.command()
+async def sync(ctx : commands.Context, *args):
+		fmt = await ctx.bot.tree.sync()
+		await ctx.send(f'Synced {len(fmt)} commands to current guild')
+		return
+
 @client.event
 async def on_ready():
+
 	print('We Have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_reaction_add(reaction : Reaction, user : User):
@@ -43,14 +61,22 @@ async def on_reaction_add(reaction : Reaction, user : User):
 	await rsManager.handelReaction(reaction, user)
 
 async def main():
+	TYPE_CHECKING = False
 	print("Starting RSBot")
 
 	# HelpCommand('help', cm)
 
 	await client.load_extension(name="RSQueueManager")
 
-	for folder in os.listdir("modules"):
-		if os.path.exists(os.path.join("modules", folder, "cog.py")):
+	if __debug__:
+		modules = '.\\src\\modules'
+		print(f'{modules}')
+	else:
+		modules = 'modules'
+		print(f'modules')
+
+	for folder in os.listdir(modules):
+		if os.path.exists(os.path.join(modules, folder, "cog.py")):
 			await client.load_extension(name=f"modules.{folder}.cog")
 
 	sBotInfo : BotInfo = client.get_cog("BotInfo")
--- src/modules/BotInfo/cog.py
+++ src/modules/BotInfo/cog.py
@@ -1,8 +1,9 @@
 import asyncio
 
 import discord
-from discord import User, Member, DMChannel, Reaction
+from discord import User, Member, DMChannel, Reaction, app_commands
 from discord.ext import commands, tasks
+
 
 from datetime import datetime
 
@@ -17,13 +18,34 @@ class BotInfo(commands.Cog, name="BotInfo"):
 		self.bot = bot
 		self.softwareId = ""
 
+
 	def setSoftwareId(self, sid : str):
 		self.softwareId = sid
+
+
+	
+	@app_commands.command(name='test', description='describe test')
+	@app_commands.describe(name='name to say hello')
+	async def test(self, interaction: discord.Interaction, name: str):
+		await interaction.response.send_message(f"hello {name}!")
+
 	
 	@commands.command()
 	async def ver(self, ctx : commands.Context, *args):
 		await ctx.send(self.softwareId)
 
+	@commands.command()
+	async def sync_slash(self, ctx : commands.Context, *args):
+			fmt = await ctx.bot.tree.sync(guild = discord.Object(id=939859311847415818))
+			await ctx.send(f'Synced {len(fmt)} commands to guild')
+			return
+
+	@commands.command()
+	async def clear_slash(self, ctx : commands.Context, *args):
+			ctx.bot.tree.clear_commands(guild = discord.Object(id=939859311847415818))
+			await ctx.send(f'Cleared slash commands from guild')
+			return
+
 async def setup(bot : commands.bot):
     print ("Addint BotInfo Cog")
-    await bot.add_cog(BotInfo(bot))
+    await bot.add_cog(BotInfo(bot), guilds = [discord.Object(id=939859311847415818)])
